a77610b3c27c6d7141c8bb224972eaa661704703
Noein <noein93@gmail.com>
2013-02-24 23:14:01 +0400

    Add license notes

--- src/interface/mainwindow.rb
+++ src/interface/mainwindow.rb
@@ -1,7 +1,23 @@
 # coding: UTF-8
 
+##
 # Copyright (C) 2013 Vladislav Mileshkin
-# 
+#
+# This file is part of TMIS.
+#
+# TMIS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TMIS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TMIS. If not, see <http://www.gnu.org/licenses/>.
+#
 
 require 'Qt'
 require './src/interface/ui_mainwindow'
