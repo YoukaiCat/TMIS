3c22d601aae0846695b842e72f82e2dd7cf0feb8
Vladislav Mileshkin <noein93@gmail.com>
2013-08-30 13:06:42 +0400

    Fix lecturer#to_s for records without required fields

--- src/engine/models/lecturer.rb
+++ src/engine/models/lecturer.rb
@@ -12,7 +12,26 @@ class Lecturer < ActiveRecord::Base
 
   Contract None => String
   def to_s
-    "#{surname}#{(name.nil? ? '' : " #{name[0].mb_chars.capitalize.to_s}.")}#{(patronymic.nil? ? '' : " #{patronymic[0].mb_chars.capitalize.to_s}.")}"
+    first = surname
+    if name.nil?
+      second = ""
+    else
+      if name.empty?
+        second = name
+      else
+        second = name[0].mb_chars.capitalize.to_s
+      end
+    end
+    if patronymic.nil?
+      third = ""
+    else
+      if patronymic.empty?
+        third = name
+      else
+        third = patronymic[0].mb_chars.capitalize.to_s
+      end
+    end
+    "#{surname} #{second}.#{third}."
   end
 
   def set_stubs_for_studies
